1ea238a5a9dcd3f23332a6fde7f4a27696610256
nicogon <nicogon@gmail.com>
2019-08-24 20:23:03 -0300

    dispo client

--- cliente medidor/dispo_proyecto/dispo_proyecto.ino
+++ cliente medidor/dispo_proyecto/dispo_proyecto.ino
@@ -1,10 +1,19 @@
 #include <Wire.h>
 // #include <ESP32WiFi.h>
 
+#include <HTTPClient.h>
+
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <ArduinoOTA.h>
+#include <Bridge.h>
+
+
+// HttpClient client;
+
+HTTPClient http;
+
 
 //#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"'
 
@@ -16,16 +25,27 @@ WebServer server(80);
 
 Adafruit_ADS1115 ads;     // Use this for the 16-bit version
 
-const int alertPin = 2;
+const int alertPin = 13;
 
+bool disableMediciones = false;
 volatile bool continuousConversionReady = false;
-float potenciaEficaz;
 const char* ssid     = "nico5";
 const char* password = "kokokokoko";
+long ant;
+int i = 0;
+int rec = 0; int f;
+float data[1000];
 
+
+//tomo 200 mediciones q son como 1/4 de segundo
+int sampleCount = 200;
+
+
+String dispoId = "1234";
 
 void handleRoot() {
-  server.send(200, "text/plain", "{\"power\":\""+String(potenciaEficaz, 3)+"\"}");
+  server.send(200, "It's alive");
+  // server.send(200, "text/plain", "{\"power\":\"" + String(potenciaEficaz, 3) + "\"}");
 }
 
 
@@ -50,27 +70,19 @@ void handleNotFound() {
 void setup(void)
 {
   pinMode(alertPin, INPUT);
-
   Serial.begin(115200);
-
   WiFi.mode(WIFI_STA);
-
   WiFi.begin(ssid, password);
-  WiFi.setHostname("esp-cliente");
-
+  WiFi.setHostname(("cliente-medidor-" + dispoId).c_str());
 
   display.init();
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_16);
-
-
   display.clear();
-
   display.setColor(WHITE);
   display.setTextAlignment(TEXT_ALIGN_CENTER);
-  display.drawString(64, 15, "inicindo");
+  display.drawString(64, 15, "iniciando");
   display.display();
-
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
@@ -85,40 +97,38 @@ void setup(void)
     display.setColor(WHITE);
     display.setTextAlignment(TEXT_ALIGN_CENTER);
     display.clear();
-    display.drawString(64, 15, WiFi.localIP().toString());
+    display.drawString(64, 10, "Conectando...");
+
+    display.drawString(64, 15 + 11, WiFi.localIP().toString());
     display.display();
 
-    if (MDNS.begin("esp32-cliente-medidor")) {
+    if (MDNS.begin(("cliente-medidor-" + dispoId).c_str())) {
       Serial.println("MDNS responder started");
     }
 
-    
-  //update via wifi
-  
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
+    //update via wifi
+
+    ArduinoOTA.onStart([]() {
+      disableMediciones = true;
+      Serial.println("Start");
+    });
+    ArduinoOTA.onEnd([]() {
+      Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    ArduinoOTA.begin();
 
     server.on("/", handleRoot);
-
-    server.on("/inline", []() {
-      server.send(200, "text/plain", "this works as well");
-    });
 
     server.onNotFound(handleNotFound);
 
@@ -126,14 +136,11 @@ void setup(void)
     Serial.println("HTTP server started");
 
   }
-  // Serial.println("Hello!");
 
-  // The ADC input range (or gain) can be changed via the following
-  // functions, but be careful never to exceed VDD +0.3V max, or to
-  // exceed the upper and lower limits if you adjust the input range!
-  // Setting these values incorrectly may destroy your ADC!
-  //                                                                ADS1015  ADS1115
-  //                                                                -------  -------
+  delay(1000);
+
+
+
   //  ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
   ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
   // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
@@ -141,84 +148,72 @@ void setup(void)
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
 
-  //ads.begin(1,0);                // for ESP8266  SDA, SCL can be specified
-  //ads.begin();
-
-
-  //SDA SCL
   Wire.begin(5, 4);
 
-  Serial.println("Starting continous mode on A0 at 8 SPS");
   ads.setSPS(ADS1115_DR_860SPS);
   ads.startContinuous_SingleEnded(0);
-  //ads.startContinuous_SingleEnded(1);
-  //ads.startContinuous_SingleEnded(2);
-  //ads.startContinuous_SingleEnded(3);
-  //ads.startContinuous_Differential_0_1();
-  //ads.startContinuous_Differential_0_3();
-  //ads.startContinuous_Differential_1_3();
-  //ads.startContinuous_Differential_2_3();
 
-  // delay(1
   attachInterrupt(digitalPinToInterrupt(alertPin), continuousAlert, FALLING);
 }
 
+
 void continuousAlert() {
-
-  // Do not call getLastConversionResults from ISR because it uses I2C library that needs interrupts
-  // to make it work, interrupts would need to be re-enabled in the ISR, which is not a very good practice.
-
   continuousConversionReady = true;
 }
-long ant;
-int i = 0;
-int rec = 0; int f;
-float data[1000];
+
+void reportar(float potenciaEficaz) {
+  http.begin("http://192.168.0.14:3000/dispositivos/1234/report");
+  http.addHeader("Content-Type", "application/json");
+  int code = http.POST("{\"medicion\":" + String(potenciaEficaz, 1) + ",\"pinza\":35,\"dispoId\":\"" + dispoId + "\"}");
+  http.writeToStream(&Serial);
+  http.end();
+}
+
+float calcularValorCorriente() {
+
+  float valorEficaz = 0;
+  for (int a = 0; a < sampleCount; a++) {
+    valorEficaz += data[a] * data[a] / sampleCount;
+  }
+  float valor = sqrt(valorEficaz) * 0.94;
+  return valor > 1 ? valor : 0;
+}
+
+float calcularValorPotencia(float corrienteEficaz) {
+  return corrienteEficaz * 220;
+}
+
+
+void actualizarDisplay(float potenciaEficaz, float corrienteEficaz) {
+  display.clear();
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(64, 5, String(potenciaEficaz, 2) + "w");
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(64, 32, String(corrienteEficaz, 2) + "a");
+  display.display();
+}
+
+
 void loop(void)
 {
-
+  ArduinoOTA.handle();
   server.handleClient();
 
-
-  if (continuousConversionReady) {
-
+  if (!disableMediciones && continuousConversionReady) {
     data[i] = ((float) ads.getLastConversionResults()) * ads.voltsPerBit() * 5; //valor en corriente
     i++;
     continuousConversionReady = false;
 
-
-    int sampleCount = 200;
-
-    //tomo 200 mediciones q son como 1/4 de segundo
     if ((millis() - ant) > sampleCount) {
-
-      int a;
-
-      float valorEficaz = 0;
-      for (a = 0; a < sampleCount; a++) {
-        valorEficaz += data[a] * data[a] / sampleCount;
-      }
-
-      float corrienteEficaz = sqrt(valorEficaz) * 0.94;
-      potenciaEficaz = corrienteEficaz * 220;
-
-
-      display.clear();
-      display.drawString(64, 15, String(potenciaEficaz, 3) + "w " +  String(corrienteEficaz, 3) + "a");
-      display.display();
-
-
-
+      float corrienteEficaz = calcularValorCorriente();
+      float potenciaEficaz = calcularValorPotencia(corrienteEficaz);
+      actualizarDisplay(potenciaEficaz, corrienteEficaz);
       i = 0;
-      // Serial.println(millis()-ant);
-
-      delay(4000);
+      reportar(potenciaEficaz);
+      delay(1000);
       ant = millis();
-
-
-      //  Serial.println(millis());
     }
   }
-
-
 }
+
+
