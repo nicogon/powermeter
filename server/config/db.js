3e09d6ca74fe660db1575c6af1665cf29081bf57
Lucas Voboril <lucas.voboril@wolox.com.ar>
2019-09-12 01:03:13 -0300

    Config folder

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+const config = require('./../config').common.database;
+
+module.exports = {
+  development: {
+    username: config.username,
+    password: config.password,
+    database: config.name,
+    host: config.host,
+    dialect: 'postgres',
+    logging: true
+  }
+};
