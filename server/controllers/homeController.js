52a0994964eb71c50bc1632e89b698f2df3a4305
Lucas Voboril <lucas.voboril@wolox.com.ar>
2019-09-17 18:33:27 -0300

    Delete controllers with mongo

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,23 +1,12 @@
-// const homeService = require('../services/homeService');
-
-// async function home(req, res) {
-//   res.render('home', { });
-// }
-
-// module.exports = { home };
-
-
-
 module.exports = function simulationsController(reportsService, homeService, simulationsService) {
   return {
     home
   };
 
   async function home(req, res) {
-      const reports = (await reportsService.list());
-      const simulations = (await simulationsService.list());
+    const reports = (await reportsService.list());
+    const simulations = (await simulationsService.list());
 
-      res.render('home', { reports, simulations });
-    }
+    res.render('home', { reports, simulations });
+  }
 };
-
