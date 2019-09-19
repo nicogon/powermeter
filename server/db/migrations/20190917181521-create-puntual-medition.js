4f69febc0d32a8c4eb3ea28a3c505bf896d66c16
Lucas Voboril <lucas.voboril@wolox.com.ar>
2019-09-18 23:46:50 -0300

    Sensor model migration and seed (#9)

--- server/db/migrations/20190917181521-create-puntual-medition.js
+++ server/db/migrations/20190917181521-create-puntual-medition.js
@@ -7,7 +7,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      device_id: {
+      deviceId: {
         primaryKey: true,
         type: Sequelize.INTEGER,
         references: { model: 'Devices', key: 'id' }
