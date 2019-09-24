1f7167965045be6d4ead0a1cc3263c10e32fd721
Lucas Voboril <lucas.voboril@wolox.com.ar>
2019-09-24 02:16:43 -0300

    Repair Android (#15)

--- android-emulator/app/src/main/java/com/example/lucas/powermeterdevise/CallAPI.java
+++ android-emulator/app/src/main/java/com/example/lucas/powermeterdevise/CallAPI.java
@@ -1,10 +1,43 @@
 package com.example.lucas.powermeterdevise;
 
-import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.EditText;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class CallAPI {
+public class CallAPI extends AsyncTask<String, String, Void> {
 
+    public static void requestHttp(String url, String mensuration) {
+        CallAPI service = new CallAPI();
+        service.doInBackground(url,mensuration);
+    }
+
+    @Override
+    protected Void doInBackground(String... params) {
+        String urlString = params[0]; // URL to call
+        String data = params[1]; //data to post
+        OutputStream out = null;
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+            writer.write(data);
+            writer.flush();
+            writer.close();
+            out.close();
+
+            urlConnection.connect();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
