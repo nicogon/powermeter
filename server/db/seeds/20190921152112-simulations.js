421a65cd0c9d5a275db412a2d1ae39bcc502588c
Ivan Bober <ivibober@gmail.com>
2019-10-03 19:54:34 -0300

    Agrego seeds y arreglo el centrado en detalle de simulacion

--- server/db/seeds/20190921152112-simulations.js
+++ server/db/seeds/20190921152112-simulations.js
@@ -2,12 +2,12 @@ module.exports = {
   up(queryInterface, _Sequelize) {
     return queryInterface.bulkInsert('Simulations', [
       {
-        name: 'Cocina de Nico',
-        totalCost: 1000,
-        totalKwh: 100,
-        fixedCost: 800,
-        kwhCost: 750,
-        durationInHours: 24
+        name: 'Casa de Ivan',
+        totalCost: 2664,
+        totalKwh: 703,
+        fixedCost: 1555,
+        kwhCost: 3.7950,
+        durationInHours: 720
       }
     ], {});
   },
