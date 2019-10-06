4f882e96c8c1c15021225b08c75f4e77c180637c
Ivan Bober <ivibober@gmail.com>
2019-10-06 13:25:38 -0300

    Mejoras en tablas

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -5,9 +5,11 @@ module.exports = function simulationsController(reportsService, homeService, sim
 
   async function home(req, res) {
     const allReports = (await reportsService.list());
-    const simulations = (await simulationsService.list());
+    const allSimulations = (await simulationsService.list());
 
     const reports = allReports.slice(Math.max(allReports.length - 4, 1));
+    const simulations = allSimulations.slice(Math.max(allSimulations.length - 10, 1));
+
     res.render('home', { reports, simulations });
   }
 };
