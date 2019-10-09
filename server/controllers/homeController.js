46acbb4849e71775ac7380b2919d828cf6cfd93e
nicogon <nicogon@gmail.com>
2019-10-08 21:41:07 -0300

    arreglando un bug RE RE RE complejo

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -7,8 +7,8 @@ module.exports = function simulationsController(reportsService, homeService, sim
     const allReports = (await reportsService.list());
     const allSimulations = (await simulationsService.list());
 
-    const reports = allReports.slice(Math.max(allReports.length - 4, 1));
-    const simulations = allSimulations.slice(Math.max(allSimulations.length - 10, 1));
+    const reports = allReports.slice(Math.max(allReports.length - 4, 0));
+    const simulations = allSimulations.slice(Math.max(allSimulations.length - 10, 0));
 
     res.render('home', { reports, simulations });
   }
